c5565ddcb2a32eb3d0b6833c2c6b9a9eab3043e5
Joshua Davis <jdavis@xologic.com>
2018-02-07 09:43:52 -0700

    mobile Menu

--- js/master.js
+++ js/master.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
 	$("#headerBlock").load("/sggwebsite/header.html");
+
+	$('#mobileToggle').click(function (e) { 
+		$('body, mainNav').toggleClass('click');
+	});
 });
